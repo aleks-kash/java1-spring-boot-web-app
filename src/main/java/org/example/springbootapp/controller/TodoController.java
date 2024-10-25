8ce039e96b393827b2347a007d03da6c8330375e
BuildTools <unconfigured@null.spigotmc.org>
2024-10-25 22:22:43 +0300

    services - controller update

--- src/main/java/org/example/springbootapp/controller/TodoController.java
+++ src/main/java/org/example/springbootapp/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.example.springbootapp.dto.TaskHistoryResponseDto;
 import org.example.springbootapp.dto.TodoCreateDto;
 import org.example.springbootapp.dto.TodoResponseDto;
 import org.example.springbootapp.dto.TodoUpdateDto;
+import org.example.springbootapp.service.TodoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,23 +16,26 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/todos")
 public class TodoController {
+    private final TodoService todoService;
+
     @PostMapping
     public TodoResponseDto save(@Valid @RequestBody TodoCreateDto todoCreateDto) {
-        return null;
+        return todoService.save(todoCreateDto);
     }
 
     @PutMapping("/{id}")
     public TodoResponseDto updateTodo(@PathVariable Long id, @Valid @RequestBody TodoUpdateDto todoUpdateDto) {
-        return null;
+        return todoService.update(id, todoUpdateDto);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
-        return null;
+        todoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}/history")
     public List<TaskHistoryResponseDto> getTaskHistory(@PathVariable Long id) {
-        return List.of();
+        return todoService.findTaskHistory(id);
     }
 }
