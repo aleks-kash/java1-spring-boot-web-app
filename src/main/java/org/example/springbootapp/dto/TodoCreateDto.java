f9cbbe7a8f52fd21304c061edae25e79d82a366f
BuildTools <unconfigured@null.spigotmc.org>
2024-10-25 21:49:13 +0300

    dto validation

--- src/main/java/org/example/springbootapp/dto/TodoCreateDto.java
+++ src/main/java/org/example/springbootapp/dto/TodoCreateDto.java
@@ -2,18 +2,29 @@ package org.example.springbootapp.dto;
 
 import org.example.springbootapp.model.Priority;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
 import java.time.LocalDateTime;
 
 public record TodoCreateDto (
+
+        @NotBlank(message = "Title can't be empty")
+        @Length(max = 100, message = "Title length must not exceed 100 characters")
         String title,
+
+        @Size(max = 500, message = "Description must be limited to 500 characters")
         String description,
+
+        @NotNull(message = "Date can't be empty")
+        @Future(message = "Due date cannot be in the past")
         LocalDateTime dueDate,
+
         Priority priority
 ) {
-/**
- * -	title: String (required, limited to 100 characters, e.g., max length = 100)
- * -	description: String (optional, but if present, limited to 500 characters)
- * -	dueDate: LocalDateTime (required)
- * -	priority: enum (optional, can be values like LOW, MEDIUM, HIGH)
- */
+
 }
