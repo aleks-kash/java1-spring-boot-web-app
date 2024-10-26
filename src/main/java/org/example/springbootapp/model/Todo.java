312176f63292024d446fc6f610a1784323fec6d2
BuildTools <unconfigured@null.spigotmc.org>
2024-10-26 16:33:40 +0300

    liquibase

--- src/main/java/org/example/springbootapp/model/Todo.java
+++ src/main/java/org/example/springbootapp/model/Todo.java
@@ -45,5 +45,6 @@ public class Todo {
     @OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<TaskHistory> taskHistories = new ArrayList<>();
 
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
     private boolean isDeleted = false;
 }
