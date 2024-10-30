a6a5ff9cc90b67d29556cdb57ea4e7a26d7692fa
BuildTools <unconfigured@null.spigotmc.org>
2024-10-30 20:30:13 +0200

    jwt

---
+++ src/main/java/org/example/springbootapp/controller/AuthController.java
@@ -0,0 +1,33 @@
+package org.example.springbootapp.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.springbootapp.dto.user.UserLoginRequestDto;
+import org.example.springbootapp.dto.user.UserLoginResponseDto;
+import org.example.springbootapp.dto.user.UserRegistrationRequest;
+import org.example.springbootapp.dto.user.UserResponseDto;
+import org.example.springbootapp.exception.RegistrationException;
+import org.example.springbootapp.security.AuthenticationService;
+import org.example.springbootapp.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto responseDto) {
+        return authenticationService.authenticate(responseDto);
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@Valid @RequestBody UserRegistrationRequest request) throws RegistrationException {
+        return userService.register(request);
+    }
+}
